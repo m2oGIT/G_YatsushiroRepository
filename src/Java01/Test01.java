8d4622d17dccf3357c8ce289fe721d5fed7b2e54
yatsushiro.genya <yatsushiro.genya@SasakiYa-PC.eiwa.local>
2016-05-23 16:32:03 +0900

    初期コミット

---
+++ src/Java01/Test01.java
@@ -0,0 +1,11 @@
+package Java01;
+
+public class Test01 {
+	public static void main(String[] args){
+		try{
+			System.out.println("テスト1だよ");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Java01/Test02.java
@@ -0,0 +1,11 @@
+package Java01;
+
+public class Test02 {
+	public static void main(String[] args){
+		try{
+			System.out.println("テスト2だよ");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
